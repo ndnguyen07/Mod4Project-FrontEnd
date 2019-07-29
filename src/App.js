07eccb9173eddc0734fc2585efab61bb33dd3896
ndnguyen07 <github email address>
2019-07-29 14:34:23 -0700

    we broke it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import './style.css';
-import NavBar from './components/NavBar';
+import Home from './components/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 /* HOME PAGE */
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div>
 
-        <NavBar />
+        <Home />
 
         {/* Signup/Login Component*/}
         {/* AllCharacters Component */}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,38 @@
 import React, { Component }from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './NavBar';
 import AllCharacters from '../containers/AllCharacters';
 
 class Home extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            characters: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3000/characters')
+        .then(resp => resp.json())
+        .then(json => {
+            this.setState({
+                characters: json
+            })
+        })
+    }
+
+
     render(){
             {/* <div className="image-container">
           <img className="image-style" src="https://challenging.events/wp-content/uploads/2017/03/superhero-pow-signs.jpeg"></img>
             </div> */}
         return(  
             <div className="title">
+            {/* <NavBar /> */}
             Super POW!!
             <AllCharacters />
-                
+            
             </div>
         )
             
